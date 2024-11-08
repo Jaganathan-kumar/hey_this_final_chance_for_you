cf70e3c5906b7ee97c66e98f1be514838283fb72
jagan12345678 <jagankumar0813@gmail.com>
2024-11-08 22:01:16 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./src/Header";
+
+
+const AppLayout =()=>{
+    return(
+        <div className="app">
+            <Header />
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />)
---
+++ src/Header.js
@@ -0,0 +1,20 @@
+const Header =() =>{
+    return(
+        <div className="nav-containe">
+            <div className="logo-container">
+                <img ClassName="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0byS4i_Yw5vi8r-Eu-mDk57CMe5kiDI1cTw&s" alt="" />
+            </div>
+            <div className="nav-items">
+                <ul>
+                <li>Home</li>
+                <li>Cart</li>
+                <li>Ordered</li>
+                <li>Services</li>
+                <li>About Us</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
